7a53116302257113a5c8d1693eda60d2d380193d
reinhart2013 <mduduzi.bhebhe1@icloud.com>
2025-09-13 19:57:38 +0100

    Initial Commit

---
+++ foosball/foosballApp.swift
@@ -0,0 +1,17 @@
+//
+//  foosballApp.swift
+//  foosball
+//
+//  Created by Mduduzi Bhebhe on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct foosballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
